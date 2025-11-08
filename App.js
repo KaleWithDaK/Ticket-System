fd795154019c924b834ad4501a1cc0cc75cb6e70
KaleWithDaK <karliskokarlo@gmail.com>
2025-11-08 17:52:39 +0100

    Updated to React 19.2.0

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaViewBase} from 'react-native';
+import { SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatsusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+       
+      </View>
+    </SafeAreaProvider>
+    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#466339ff',
     alignItems: 'center',
     justifyContent: 'center',
   },
